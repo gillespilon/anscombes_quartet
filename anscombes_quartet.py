6f9d4beac183f07177ae4957c030defa35838ed4
Gilles Pilon <gillespilon13@gmail.com>
2019-11-21 16:58:31 -0600

    Initial commit

---
+++ anscombes_quartet.py
@@ -0,0 +1,11 @@
+#! /usr/bin/env python3
+
+
+'''
+Anscombe's quartet
+
+- Create Anscombe's quartet of four graphs as four axes in one figure.
+- Create Anscombe's quartet of four graphs as four separate figures.
+
+./anscombes_quartet.py
+'''
