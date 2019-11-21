4c16e79ac24afa8b99b9360431f0dcb826054832
Gilles Pilon <gillespilon13@gmail.com>
2019-11-21 16:59:55 -0600

    Add librairies

--- anscombes_quartet.py
+++ anscombes_quartet.py
@@ -9,3 +9,15 @@ Anscombe's quartet
 
 ./anscombes_quartet.py
 '''
+
+
+import pandas as pd
+import matplotlib.pyplot as plt
+from numpy.polynomial import polynomial as nppoly
+import matplotlib.cm as cm
+import matplotlib.axes as axes
+from matplotlib.gridspec import GridSpec
+
+
+get_ipython().run_line_magic('matplotlib', 'inline')
+get_ipython().run_line_magic('config', "InlineBackend.figure_format = 'svg'")
